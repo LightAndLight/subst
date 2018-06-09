6b19fd77d7e30612ba3cbd05fb9d1c8a9fd88a42
Isaac Elliott <isaace71295@gmail.com>
2018-06-09 12:51:59 +1000

    Added documentation

--- example/subst-example.nix
+++
@@ -1,8 +0,0 @@
-{ mkDerivation, base, subst, stdenv }:
-mkDerivation {
-  pname = "subst-example";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base subst ];
-  license = stdenv.lib.licenses.bsd3;
-}
--- src/Language/Subst.hs
+++ src/Language/Subst.hs
@@ -9,6 +9,8 @@ import Control.Lens.Setter (over)
 
 import Language.Subst.Indef
 
+-- | Abstract over a particular free variable in a 'Term'
+-- and wrap the result in a binder
 abstract :: F -> Term -> Term
 abstract v = (_Binder #) . fun 0
   where
@@ -19,6 +21,12 @@ abstract v = (_Binder #) . fun 0
           | otherwise -> t
         _ -> over plate (fun $ maybe n (\_ -> n+1) (t ^? _Binder)) t
 
+-- | Attempt to apply a value to a function
+--
+-- If @f@ is not a binder then @'instantiate' f x@ returns 'Nothing'
+--
+-- Otherwise, it returns 'Just', with @x@ substituted appropriately throughout
+-- the body of @f@
 instantiate :: Term -> Term -> Maybe Term
 instantiate f x = fun 0 <$> f ^? _Binder
   where
